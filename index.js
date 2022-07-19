afef2ed57dce6974ab089923220e12352fd49208
Matt <matthew.t.kissinger@gmail.com>
2022-07-18 20:27:38 -0700

    first version complete with F/C toggle button

--- index.js
+++ index.js
@@ -2,15 +2,24 @@
 
 // open weather API key: befd87c0ecdf2bcb8020b97c9012c6d5
 
+// TODO
+// Add in a toggle for F to C unit of measurement
+
 // global variables
 
 let Fahrenheight = 'imperial';
 let Celsius = 'metric';
 
+let uom = Celsius;
+
+let userCity = 'London';
+
 // DOM cache
 let cityName = document.querySelector('.city-name');
 
 let cityTemp = document.querySelector('.city-temp');
+
+let uomToggleBtn = document.querySelector('.uom-toggle--btn');
 
 let userCityInput = document.querySelector('#city-name');
 
@@ -20,24 +29,48 @@ let weatherImg = document.querySelector('.weather-img');
 
 
 // initial render on page load
-getData('London', Celsius);
+getData(userCity, uom);
 cityName.innerText = 'London';
 
 cityInput.addEventListener('submit', (e) => {
     e.preventDefault();
-    let userCity = userCityInput.value;
+    userCity = userCityInput.value;
 
-    getData(userCity, Celsius);
+    getData(userCity, uom);
     cityName.innerText = `${userCity}`;
+})
+
+uomToggleBtn.addEventListener('click', () => {
+    toggleUom();
 })
 
 // gets all of the json data for a given city 
 async function getData(city, uom) {
-    let data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=befd87c0ecdf2bcb8020b97c9012c6d5&units=${uom}`);
-    let dataJson = await data.json();
+    try {
+        let data = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=befd87c0ecdf2bcb8020b97c9012c6d5&units=${uom}`);
+        let dataJson = await data.json();
+        console.log(dataJson);
 
-    cityTemp.innerText = `${dataJson.main.temp}°C`;
-    weatherImg.src = `http://openweathermap.org/img/wn/${dataJson.weather[0].icon}@2x.png`
+        cityTemp.innerText = `${dataJson.main.temp} °${uom === 'metric' ? 'C' : 'F'}`;
+        weatherImg.src = `http://openweathermap.org/img/wn/${dataJson.weather[0].icon}@2x.png`;
+    } catch (err) {
+        alert(err);
+    }
+}
+
+function toggleUom() {
+
+    if (uom === 'metric') {
+        uom = 'imperial';
+        uomToggleBtn.style.marginTop = '.8rem';
+        uomToggleBtn.style.backgroundColor = 'black';
+    } else {
+        uom = 'metric';
+        uomToggleBtn.style.marginTop = '.25rem';
+        uomToggleBtn.style.backgroundColor = '#f1faee';
+    }
+
+    getData(userCity, uom);
 }
 
 
